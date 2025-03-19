c3a5004b15137c562e3542d1a68747f702c7fafe
ritambanik <ritam.banik@gmail.com>
2025-03-19 21:29:55 +0000

    Print

--- project_with_test/titanic_model/predict.py
+++ project_with_test/titanic_model/predict.py
@@ -31,6 +31,7 @@ def make_prediction(*,input_data:Union[pd.DataFrame, dict]) -> dict:
     results = {"predictions": None, "version": _version, "errors": errors}
     
     predictions = titanic_pipe.predict(validated_data)
+    print(f"Predictions = {predictions[0]}")
 
     results = {"predictions": predictions,"version": _version, "errors": errors}
     print(results)
@@ -38,7 +39,7 @@ def make_prediction(*,input_data:Union[pd.DataFrame, dict]) -> dict:
 
         predictions = titanic_pipe.predict(validated_data)
         results = {"predictions": predictions,"version": _version, "errors": errors}
-        #print(results)
+        print(results)
 
     return results
 
